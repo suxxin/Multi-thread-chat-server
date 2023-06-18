1799df68a4ede7d62fe0be55de8ff53be03d2427
Sujin <suxxin0@gmail.com>
2023-06-18 18:57:26 -0400

    #2 - feat: multiple clients server

--- src/chat_server.cpp
+++ src/chat_server.cpp
@@ -3,6 +3,38 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <thread>
+#include <unordered_map>
+
+std::unordered_map<std::string, std::string> client_map; // maps < sin_addr:port -> client # >
+
+void handle_client(int client_socket, std::string sin_addr, std::string sin_port) {
+    char client_message[1000];
+    int read_size;
+
+    // new client 
+    std::cout << "New client connected: " << sin_addr << ":" << sin_port << std::endl;
+    std::string client_addr_port = sin_addr + ":" + sin_port;
+    client_map[client_addr_port] = "client" + std::to_string(client_map.size());
+
+    // receive a message from client
+    while ((read_size = recv(client_socket, client_message, 1000, 0)) > 0) {
+        // log client's message 
+        // TODO: more prfessional log methoid? 
+        std::cout << client_map[client_addr_port] << ":" << client_message << std::endl;
+        // echo back to the client
+        write(client_socket, client_message, strlen(client_message));
+        memset(client_message, 0, 1000);
+    }
+
+    if (read_size == 0) {
+        std::cout << "Client disconnected" << std::endl;
+    } else if (read_size == -1) {
+        perror("Recv failed");
+    }
+
+    close(client_socket);
+}
 
 int main() {
     int server_socket, client_socket;
@@ -39,26 +71,13 @@ int main() {
     std::cout << "Waiting for incoming connections..." << std::endl;
     int c = sizeof(struct sockaddr_in);
     // accept
-    while ( (client_socket = accept(server_socket, (struct sockaddr *)&client, (socklen_t*)&c))) {
-        char client_message[1000];
-        int read_size;
-        
-        // receive a message from client
-        while ((read_size = recv(client_socket, client_message, 1000, 0)) > 0) {
-            // log client's message 
-            // TODO: more prfessional log methoid? 
-            std::cout << client_message << std::endl;
-            // echo back to the client
-            write(client_socket, client_message, strlen(client_message));
-            memset(client_message, 0, 1000);
-        }
-
-        if (read_size == 0) {
-            std::cout << "Client disconnected" << std::endl;
-        } else if (read_size == -1) {
-            perror("Recv failed");
-            break;
-        }
+    while ( (client_socket = accept(server_socket, (struct sockaddr *)&client, (socklen_t*)&c)) ) {
+        // the arguments are "copied" to the new thread
+        std::string sin_addr = std::string(inet_ntoa(client.sin_addr));
+        std::string sin_port = std::to_string(ntohs(client.sin_port));
+        // create a thread
+        std::thread client_thread(handle_client, client_socket, sin_addr, sin_port);
+        client_thread.detach(); // individually process each client
     }
     close(server_socket);
     return 0;
